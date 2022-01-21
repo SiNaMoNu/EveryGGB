90fc8cf90114bffde3d5e4cf48c5ca035544999d
SatelliteSilver <42998574+SatelliteSilver@users.noreply.github.com>
2022-01-22 04:28:36 +0900

    Update Functions.java
    들여쓰기 장인 강림.!

--- Functions.java
+++ Functions.java
@@ -8,59 +8,54 @@ import java.net.UnknownHostException;
 
 
 public class Functions {
-	   Socket requestSocket;
-	   ObjectOutputStream out;
-	   ObjectInputStream in;
-	   String message;
+	Socket requestSocket;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String message;
 	    
-	   public Functions(){}
-	   public void sign_up(String id, String password)
-	   {
-	      try{
+	public Functions(){}
+	public void sign_up(String id, String password)
+	{
+		try{
+	        requestSocket = new Socket("1.201.151.37", 2004);
 	         
-	         requestSocket = new Socket("1.201.151.37", 2004);
+	        out = new ObjectOutputStream(requestSocket.getOutputStream());
+	        out.flush();
+	        in = new ObjectInputStream(requestSocket.getInputStream());
+	        //3: Communicating with the server
 	         
-	         
-	         out = new ObjectOutputStream(requestSocket.getOutputStream());
-	         out.flush();
-	         in = new ObjectInputStream(requestSocket.getInputStream());
-	         //3: Communicating with the server
-	         
-	         try{
-	            out.writeObject("SUC");
-	            out.flush();
-	            out.writeObject(id);
-	            out.flush();
-	            out.writeObject(password);
-	            out.flush();
-	            
-	         }
-	         catch(IOException ioException){
-	            ioException.printStackTrace();
-	         }
-	         
-	      }
-	      catch(UnknownHostException unknownHost){
-	         System.err.println("You are trying to connect to an unknown host!");
-	      }
-	      catch(IOException ioException){
-	         ioException.printStackTrace();
-	      }
-	      finally{
-	         //4: Closing connection
-	         try{
-	            in.close();
-	            out.close();
-	            requestSocket.close();
-	         }
-	         catch(IOException ioException){
-	            ioException.printStackTrace();
-	         }
-	      }
-	      
-	   }
+			try{
+				out.writeObject("SUC");
+				out.flush();
+				out.writeObject(id);
+				out.flush();
+				out.writeObject(password);
+				out.flush();
+			 }
+			 catch(IOException ioException){
+				    ioException.printStackTrace();
+			 }        
+		}
+		catch(UnknownHostException unknownHost){
+			System.err.println("You are trying to connect to an unknown host!");
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		finally{
+			 //4: Closing connection
+			try{
+			    in.close();
+			    out.close();
+			    requestSocket.close();
+			}
+			catch(IOException ioException){
+				ioException.printStackTrace();
+			}
+		}
+	}
 	   
-void call_SUU(String password)
+	void call_SUU(String password)
 	{
 		try{
 			
@@ -105,10 +100,10 @@ void call_SUU(String password)
 		
 	}
 	
-	   public static void main(String[] args) throws IOException {
-	      String id = "";
-	      String password = "";
-	      Functions client = new Functions();
-	      client.sign_up(id, password);
-	   }
+	public static void main(String[] args) throws IOException {
+		String id = "";
+		String password = "";
+		Functions client = new Functions();
+		client.sign_up(id, password);
+	}
 }
