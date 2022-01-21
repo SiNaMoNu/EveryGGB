84cb072d4e2dc661fc69ab5a7cdc5ce06ca92920
Park Seungbeom <81613308+SiNaMoNu@users.noreply.github.com>
2022-01-22 03:20:17 +0900

    Create Functions.java

---
+++ Functions.java
@@ -0,0 +1,69 @@
+package application.model;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class Functions {
+	Socket requestSocket;
+	   ObjectOutputStream out;
+	    ObjectInputStream in;
+	    String message;
+	    
+	   public Functions(){}
+	   public void sign_up(String id, String password)
+	   {
+	      try{
+	         
+	         requestSocket = new Socket("1.201.151.37", 2004);
+	         
+	         
+	         out = new ObjectOutputStream(requestSocket.getOutputStream());
+	         out.flush();
+	         in = new ObjectInputStream(requestSocket.getInputStream());
+	         //3: Communicating with the server
+	         
+	         try{
+	            out.writeObject("SUC");
+	            out.flush();
+	            out.writeObject(id);
+	            out.flush();
+	            out.writeObject(password);
+	            out.flush();
+	            
+	         }
+	         catch(IOException ioException){
+	            ioException.printStackTrace();
+	         }
+	         
+	      }
+	      catch(UnknownHostException unknownHost){
+	         System.err.println("You are trying to connect to an unknown host!");
+	      }
+	      catch(IOException ioException){
+	         ioException.printStackTrace();
+	      }
+	      finally{
+	         //4: Closing connection
+	         try{
+	            in.close();
+	            out.close();
+	            requestSocket.close();
+	         }
+	         catch(IOException ioException){
+	            ioException.printStackTrace();
+	         }
+	      }
+	      
+	   }
+	   
+	   public static void main(String[] args) throws IOException {
+	      String id = "";
+	      String password = "";
+	      Functions client = new Functions();
+	      client.sign_up(id, password);
+	   }
+}
